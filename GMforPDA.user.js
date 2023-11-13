ab3963a3724479d466baa78c4d1a434c17b03e3c
Blair <87805559+Kwack-Kwack@users.noreply.github.com>
2023-11-13 10:54:04 +0800

    v0.2

--- GMforPDA.user.js
+++ GMforPDA.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GMforPDA
 // @namespace    https://github.com/Kwack-Kwack/GMforPDA
-// @version      0.1
+// @version      0.2
 // @description  A userscript that allows GM_ functions to be called in tornPDA. Simply replace the underscore (_) with a period (.) eg GM.addStyle
 // @author       Kwack [2190604]
 // @match        https://*
@@ -14,44 +14,152 @@
     - getValue / setValue is global and NOT script-specific. Use a unique storage key to prevent potential clashes with other scripts.
 */
 
+const ver = 0.2;
+
+if (!window.flutter_inappwebview)
+	throw new Error(
+		"GMforPDA requires flutter_inappwebview to be defined. Ensure this script is running inside of PDA."
+	);
+
 window.GM = {
-    getValue(key, defaultValue) {
-        return localStorage.getItem(key) ?? defaultValue;
-    },
+	/**
+	 * To enforce a script version, throw an error if window.GM.ver !== [desired version]
+	 * To enforce only a major version (minor, unbreaking changes permitted) just use Math.floor(window.GM.ver) !== [desired major version]
+	 */
+	ver,
 
-    setValue(key, value) {
-        localStorage.setItem(key, value);
-    },
+	/**
+	 * @param {string} key The key to the specified value.
+	 * @param {string} defaultValue The value to be returned if there is no value associated with the specified key
+	 * @returns {string} Returns the value associated with the specified key, or the default value if there is no value associated with the specified key.
+	 */
+	getValue(key, defaultValue) {
+		return localStorage.getItem(key) ?? defaultValue;
+	},
 
-    deleteValue(key) {
-        localStorage.removeItem(key);
-    },
+	/**
+	 * @param {string} key The key to store the value under. This is global and NOT script-specific. Use a unique storage key to prevent potential clashes with other scripts.
+	 * @param {string} value The value to store under the specified key.
+	 * @returns {void}
+	 */
+	setValue(key, value) {
+		localStorage.setItem(key, value);
+	},
 
-    addStyle(style) {
-        const s = document.createElement("style");
-        s.type = "text/css";
-        s.innerHTML = style;
+	/**
+	 * @param {string} key Removes a key-value pair from storage.
+	 * @returns {void}
+	 */
+	deleteValue(key) {
+		localStorage.removeItem(key);
+	},
 
-        document.head.appendChild(s);
-    },
+	/**
+	 * @returns {string[]} Returns an array of all keys in storage.
+	 */
+	listValues() {
+		return Object.values(localStorage);
+	},
 
-    setClipboard(text) {
-        navigator.clipboard.writeText(text);
-    },
+	/**
+	 *
+	 * @param {string} style The CSS to be added to the page, as a string.
+	 * @returns {void}
+	 */
+	addStyle(style) {
+		const s = document.createElement("style");
+		s.type = "text/css";
+		s.innerHTML = style;
 
-    async xmlhttpRequest(details) {
-        if (!details.method || details.method.toLowerCase() === "get") {
-            return await PDA_httpGet(details.url)
-                .then(details.onload)
-                .catch(details.onerror ?? ((e) => console.error(e)));
-        } else if (details.method.toLowerCase() === "post") {
-            return await PDA_httpPost(
-                details.url,
-                details.headers ?? {},
-                details.body ?? ""
-            )
-                .then(details.onload)
-                .catch(details.onerror ?? ((e) => console.error(e)));
-        } else throw new Error("Invalid method passed to GM.xmlHttpRequest");
-    },
+		document.head.appendChild(s);
+	},
+
+	/**
+	 *
+	 * @description Only works if the document is focused.
+	 * @param {string} text The text to be copied to the clipboard.
+	 * @returns {void}
+	 */
+	setClipboard(text) {
+		if (!document.hasFocus()) return;
+		navigator.clipboard.writeText(text);
+	},
+
+	/**
+	 *
+	 * @param details The details passed to the request.
+	 * @param {"GET" | "POST"} details.method The HTTP method to use.
+	 * @param {string} details.url The URL to send the request to.
+	 * @param {string} details.data The body of the request, for POST requests
+	 * @param {Object} details.headers The headers to send with the request, as an object. eg { "Content-Type": "application/json" }
+	 * @param {function} details.onload The function to be called when the request is successful. The response is passed as an argument.
+	 * @param {function} details.onerror The function to be called when the request fails. The error is passed as an argument.
+	 * @throws {Error} Invalid method passed to GM.xmlHttpRequest
+	 * @returns {Promise} A promise that resolves when the request is successful, and rejects when the request fails. Access response body via `response.responseText` (property, not method).
+	 */
+	async xmlhttpRequest(details) {
+		if (!details.method || details.method.toLowerCase() === "get") {
+			return await PDA_httpGet(details.url)
+				.then(details.onload)
+				.catch(details.onerror ?? ((e) => console.error(e)));
+		} else if (details.method.toLowerCase() === "post") {
+			return await PDA_httpPost(
+				details.url,
+				details.headers ?? {},
+				details.body ?? details.data ?? ""
+			)
+				.then(details.onload)
+				.catch(details.onerror ?? ((e) => console.error(e)));
+		} else throw new Error("Invalid method passed to GM.xmlHttpRequest");
+	},
+
+	/**
+	 *
+	 * @param  {...any} args Either an object with the following properties, or the properties themselves in order:
+	 * @param {string} text The text to be displayed in the alert.
+	 * @param {string} title The title of the alert.
+	 * @param {function} onclick The function to be called when the alert is clicked.
+	 * @param {function} ondone The function to be called when the alert is closed.
+	 */
+	notification(...args) {
+		let text, title, onclick, ondone;
+		if (typeof args[0] === "string") {
+			[text, title, , onclick] = args;
+		} else {
+			({ text, title, onclick, ondone } = args[0]);
+		}
+		const alert =
+			(title
+				? `Notification from script ${title}:`
+				: "Notification from unnamed source:") +
+			"\n" +
+			text;
+		if (confirm(alert)) onclick?.();
+		return ondone?.();
+	},
+
+	/**
+	 *
+	 * @param {string} url the URL to open in a new tab
+	 */
+	openInTab(url) {
+		window.open(url, "_blank");
+	},
+
+	info: {
+		script: {
+			description: "This information is unavailable in TornPDA",
+			excludes: [],
+			includes: [],
+			matches: [],
+			name: undefined,
+			namespace: undefined,
+			resources: {},
+			"run-at": undefined,
+			version: undefined,
+		},
+		scriptMetaStr: "This information is unavailable in TornPDA",
+		scriptHandler: `TornPDA, using GMforPDA version ${ver}`,
+		version: ver,
+	},
 };
--- README.md
+++ README.md
@@ -1,35 +1,55 @@
 # GMforPDA
+
 ### How to use
-*Please note: this userscript should be run at the document start, not document end; so it will add GM methods before other userscripts execute.*
 
-This userscript (built for [Torn PDA](https://github.com/Manuito83/torn-pda)) should allow you to use basic GreaseMonkey (GM_) functions. 
+Install this script into [Torn PDA](https://github.com/Manuito83/torn-pda/) and select a `start` injection time, to make sure it injects before any other scripts. This script will add the GM\_ functions to the window object, so you can use them as you would normally.
 
-You can use it with the functions below by replacing the underscore in the GM function with a period (e.g. `GM_addStyle` to `GM.addStyle`). This is done intentionally to force users to be aware that modification of source code **will** be required for this to work, and I do not guarantee that this will work with all GreaseMonkey userscripts. You'll also require modification for the other greasemonkey headers in the `//==Userscript==` header, as PDA only acts on the match pattern.
+This script is still in an alpha stage and has not been tested as extensively as I would have hoped. Please submit any issues/requests via the [github issues](https://github.com/Kwack-Kwack/GMforPDA/issues) or [submit a pull request](https://github.com/Kwack-Kwack/GMforPDA/pulls).
 
-Please note that mobile browsers, especially on iOS, behave differently than chromium browsers so your userscripts will not act the same as they may on PC. 
+Please note this script adds a `GM` object to the window with these methods. This means that calling the function with an underscore (eg `GM_addStyle`) will still throw an undefined error. You must use either the period syntax (eg `GM.addStyle`) or define the required functions at the top of the script (eg `const GM_addStyle = GM.addStyle;`).
 
+Many scripts will not "magically" work with this script due to requiring other reworks that come with mobile browsers. Please reach out to me if you have any questions, or if any scripts are not working as expected.
 
-It currently supports:
-#### `getValue(key, defaultValue)` / `setValue(key, value)` / `deleteValue(key)`
-Uses the browser's localStorage, please note that this is not permanent storage and may be erased over time / manually by the user.
-#### `addStyle(style)`
-Adds a stylesheet to the document's head. `style` argument should be a string.
-#### `setClipboard(text)`
-Copies supplied string to the clipboard. Use an empty string to clear the clipboard.
-#### `xmlhttpRequest(details)`
-Opens a http request.
-##### The details object must contain at least the url. Examples are below.
-```js
-await GM.xmlhttpRequest({
-  url: "https://api.example.com/",
-  method: "POST", //optional, defaults to GET
-  onload: (r) => console.log(r.responseText), //optional, executes this function after the request is complete.
-  // Argument r is the response object, access the response via r.responseText. Please note that if this should be JSON, you must call JSON.parse(r.responseText)
-  onerror: (e) => console.error(e), //optional, this is the default. Catches the error and passes it to the onerror function.
-  headers: { //optional, POST requests only. 
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(exampleData) //optional, POST requests only. Only supports strings.
-})
-```
-Submit any issues/requests via the [github issues](https://github.com/Kwack-Kwack/GMforPDA/issues) or [submit a pull request](https://github.com/Kwack-Kwack/GMforPDA/pulls).
+### API Documentation
+
+##### `ver`
+
+The version of the script. This is a string in the format `major.minor` (e.g. `1.0`). **THIS IS THE _GMforPDA_ VERSION, NOT THE SCRIPT VERSION.**
+To ensure the script version is the same major version, use `Math.floor(GM.ver) === desiredMajorVersion`.
+
+##### `getValue(key, defaultValue)` / `setValue(key, value)` / `deleteValue(key)` / `listValues()`
+
+All the above use the browser's `localStorage`. Please note that:
+
+-   These are not isolated on a script basis, and other scripts will have access to the same data. Use unique keys to avoid conflicts.
+-   This is not permanent storage and may be erased over time or by another script. It is also purged whenever the user clears their cache in PDA.
+
+##### `addStyle(style)`
+
+Same syntax as the GM\_ function. Adds a stylesheet to the document's head. `style` argument should be a string.
+
+##### `setClipboard(text)`
+
+Requires the document to be focused - the function will exit silently if not. Use an empty string to clear the clipboard. Please note that iOS will sometimes prompt the user whether to allow PDA to access the clipboard due to security concerns.
+
+##### `xmlhttpRequest(details)`
+
+The details object only accepts the following keys:
+
+-   `url` - The URL to send the request to. Required.
+-   `onload` - A function to execute after the request is complete. The response object is passed as the first argument. Required.
+-   `onerror?` - A function to execute if the request fails. The error object is passed as the first argument. Defaults to `(e) => console.error(e)`
+-   `method?` - The HTTP method to use. Defaults to `GET`. Throws an error if the method is neither `GET` nor `POST`.
+-   `headers?` - An object containing the headers to send. Defaults to an empty object.
+-   `data?` - The data to send. Defaults to an empty string. Only supports strings (stringify all JSON data). Works with either a `data` or `body` key. Only works with `POST` requests.
+
+##### `notification(...args)`
+
+Uses the native javascript `confirm()` rather than a native notification due to platform restrictions. Syntax is either `notification(text, title, null, onclick)` or `notification(arg)` where `arg` is an object with the following keys:
+
+-   `text` - The text to display in the notification. Required.
+-   `title?` - The title of the notification.
+-   `onclick?` - A function that executes when the user clicks "yes" on the prompt
+-   `ondone?` - A function that executes after the prompt is dismissed, regardless of which button was pressed.
+
+Many other GM functions are not supported due to inability to rewrite in vanilla JS.
